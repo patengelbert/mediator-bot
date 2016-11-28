023e04e3ef05fa664114ce84612840b40508834a
Ryan <rd613@imperial.ac.uk>
2016-11-28 19:51:00 +0000

    added hark_msgs

---
+++ mediator_bot/hark_msgs/cmake/hark_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
+
+_prepend_path("${hark_msgs_DIR}/.." "msg" hark_msgs_MSG_INCLUDE_DIRS UNIQUE)
+set(hark_msgs_MSG_DEPENDENCIES std_msgs)
