6f9cf48b17dc1c88cda3a1e816ef2da5d689d451
Becks <beccalouisesimpson@googlemail.com>
2016-11-28 18:48:36 +0000

    Everything working

---
+++ build/build/beginner_tutorials/CMakeFiles/beginner_tutorials_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/beginner_tutorials_generate_messages_cpp"
+  "/home/becks/mediator-bot/build/devel/include/beginner_tutorials/Num.h"
+  "/home/becks/mediator-bot/build/devel/include/beginner_tutorials/AddTwoInts.h"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/beginner_tutorials_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(beginner_tutorials_MESSAGE_FILES "/home/becks/mediator-bot/build/src/beginner_tutorials/msg/Num.msg")
+set(beginner_tutorials_SERVICE_FILES "/home/becks/mediator-bot/build/src/beginner_tutorials/srv/AddTwoInts.srv")
