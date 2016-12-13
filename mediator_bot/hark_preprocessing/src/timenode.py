51abb8658faaaf5843981b2d377edf6b8be364a8
Patrick Engelbert <patengelbert@hotmail.co.uk>
2016-12-13 21:27:19 +0000

    Fixed bug where old status was sent

--- mediator_bot/hark_preprocessing/src/timenode.py
+++ mediator_bot/hark_preprocessing/src/timenode.py
@@ -65,7 +65,7 @@ class SpeakerContainer(object):
     @status.setter
     def status(self, val):
         if self._status != val:
-            self.sendMessage()
+            self.sendMessage(val)
             rospy.logdebug("{}: Setting status - {}".format(self.label, val))
         self._status = val
 
@@ -84,13 +84,13 @@ class SpeakerContainer(object):
         else:
             self.status = Status.NO_STATUS
 
-    def sendMessage(self):
+    def sendMessage(self, status):
         msg = MedBotSpeechStatus()
         msg.header = rospy.Header()
         msg.header.stamp = rospy.Time.now()
         msg.weight = self.weight
         msg.name = self.label
-        msg.status = self.status.value
+        msg.status = status.value
         msg.speaking = self.speaking
         if len(self.azimuths) == 0:
             msg.azimuth = 0
